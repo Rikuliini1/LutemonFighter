bfb1d6e48129eec59c878b053f4eb63e3c699780
Rikuliini1 <joona.isokorpi01@gmail.com>
2023-05-02 19:15:56 +0300

    Add save and load, add home screen house

--- app/src/main/java/com/lutemonfighter/HomeActivity.java
+++ app/src/main/java/com/lutemonfighter/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.lutemonfighter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,42 +11,52 @@ import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     private EditText lutemonName;
-    private LutemonView lutemonView;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         lutemonName = findViewById(R.id.editTxtLutemonName);
-        lutemonView = findViewById(R.id.lutemonView);
+        context = HomeActivity.this;
     }
 
     public void createNewLutemon(View view) {
         RadioGroup rgLutemonTypes = findViewById(R.id.rgLutemonTypes);
         switch (rgLutemonTypes.getCheckedRadioButtonId()) {
             case R.id.rbWater:
-                Storage.getInstance().addLutemon(new WaterLutemon());
+                Storage.getInstance().addLutemon(new WaterLutemon(lutemonName.getText().toString()));
                 Toast.makeText(this, "New Water Lutemon created", Toast.LENGTH_LONG).show();
                 break;
             case R.id.rbFire:
-                Storage.getInstance().addLutemon(new FireLutemon());
+                Storage.getInstance().addLutemon(new FireLutemon(lutemonName.getText().toString()));
                 Toast.makeText(this, "New Fire Lutemon created", Toast.LENGTH_LONG).show();
                 break;
             case R.id.rbEarth:
-                Storage.getInstance().addLutemon(new EarthLutemon());
+                Storage.getInstance().addLutemon(new EarthLutemon(lutemonName.getText().toString()));
                 Toast.makeText(this, "New Earth Lutemon created", Toast.LENGTH_LONG).show();
                 break;
             case R.id.rbAir:
-                Storage.getInstance().addLutemon(new AirLutemon());
+                Storage.getInstance().addLutemon(new AirLutemon(lutemonName.getText().toString()));
                 Toast.makeText(this, "New Air Lutemon created", Toast.LENGTH_LONG).show();
                 break;
             case R.id.rbElectric:
-                Storage.getInstance().addLutemon(new ElectricLutemon());
+                Storage.getInstance().addLutemon(new ElectricLutemon(lutemonName.getText().toString()));
                 Toast.makeText(this, "New Electric Lutemon created", Toast.LENGTH_LONG).show();
                 break;
             default:
                 Toast.makeText(this, "Select a type first", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void saveLutemons(View view) {
+        Storage.getInstance().saveLutemons(context);
+        Toast.makeText(context, "Lutemons saved successfully", Toast.LENGTH_LONG).show();
+    }
+
+    public void loadLutemons(View view) {
+        Storage.getInstance().loadLutemons(context);
+        Toast.makeText(context, "Lutemons loaded successfully", Toast.LENGTH_LONG).show();
     }
 
     public void switchToHomeActivity(View view) {
--- app/src/main/java/com/lutemonfighter/Lutemon.java
+++ app/src/main/java/com/lutemonfighter/Lutemon.java
@@ -1,6 +1,8 @@
 package com.lutemonfighter;
 
-public class Lutemon {
+import java.io.Serializable;
+
+public class Lutemon implements Serializable {
     protected String name;
     protected String type;
     protected int attack;
